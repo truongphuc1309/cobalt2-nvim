78616479f92a4f9aa246a4acbe3bd08e1db828ae
Truong Phuc <truongngo1309@gmail.com>
2026-06-14 03:40:02 +0700

    feat: optimize colors for lualine

--- lua/lualine/themes/_cobalt2.lua
+++ lua/lualine/themes/_cobalt2.lua
@@ -3,43 +3,51 @@ local M = {}
 function M.get()
   local colors, config = require("cobalt2.colors").setup()
 
+  local section_bg = colors.border
+  local middle_bg = colors.bg_dark1
+
   local hl = {}
 
   hl.normal = {
     a = { bg = colors.yellow, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.yellow },
-    c = { bg = colors.bg_statusline, fg = colors.fg_sidebar },
+    b = { bg = section_bg, fg = colors.yellow },
+    c = { bg = middle_bg, fg = colors.green2 },
   }
 
   hl.insert = {
     a = { bg = colors.green, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.green },
+    b = { bg = section_bg, fg = colors.green },
+    c = { bg = middle_bg, fg = colors.green2 },
   }
 
   hl.command = {
     a = { bg = colors.yellow, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.yellow },
+    b = { bg = section_bg, fg = colors.yellow },
+    c = { bg = middle_bg, fg = colors.green2 },
   }
 
   hl.visual = {
     a = { bg = colors.magenta, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.magenta },
+    b = { bg = section_bg, fg = colors.magenta },
+    c = { bg = middle_bg, fg = colors.green2 },
   }
 
   hl.replace = {
     a = { bg = colors.red, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.red },
+    b = { bg = section_bg, fg = colors.red },
+    c = { bg = middle_bg, fg = colors.green2 },
   }
 
   hl.terminal = {
     a = { bg = colors.green1, fg = colors.black },
-    b = { bg = colors.fg_gutter, fg = colors.green1 },
+    b = { bg = section_bg, fg = colors.green1 },
+    c = { bg = middle_bg, fg = colors.green2 },
   }
 
   hl.inactive = {
-    a = { bg = colors.bg_statusline, fg = colors.yellow },
-    b = { bg = colors.bg_statusline, fg = colors.fg_gutter, gui = "bold" },
-    c = { bg = colors.bg_statusline, fg = colors.fg_gutter },
+    a = { bg = middle_bg, fg = colors.dark5 },
+    b = { bg = middle_bg, fg = colors.dark5, gui = "bold" },
+    c = { bg = middle_bg, fg = colors.dark5 },
   }
 
   if config.lualine_bold then
